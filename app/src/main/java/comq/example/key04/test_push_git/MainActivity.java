fd29e1b7c8a325d504df0c9e488ad71e1b869b5d
LEMONSHARK <key0416sh@naver.com>
2020-04-26 00:02:39 +0900

    let's git it!! please

---
+++ app/src/main/java/comq/example/key04/test_push_git/MainActivity.java
@@ -0,0 +1,13 @@
+package comq.example.key04.test_push_git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
